2d5bc4fbe5b8ba44430c6618f8aa3616395aa6ad
Kensuke Kojima <kozima@fos.kuis.kyoto-u.ac.jp>
2016-11-30 16:57:11 +0900

    Add results of experiments

--- run_noinline.sh
+++ run_noinline.sh
@@ -21,3 +21,35 @@ run matrixMul.cu matrixMul
 run matrixMul.cu matrixMul2
 run matrixMul.cu matrixMul4
 run diffusion1d.cu diffusion1d
+
+
+## output
+
+# Result: 0 1
+# VCG: 0.048402
+# SMT: 5.911971
+# Size: 1646 1
+# Result: 0 2
+# VCG: 0.098883
+# SMT: 14.039834
+# Size: 3365 2
+# Result: 4 7
+# VCG: 0.283711
+# SMT: 39.363525
+# Size: 20948 12
+# Result: 14 16
+# VCG: 2.184182
+# SMT: 26.854666
+# Size: 112714 28
+# Result: 17 20
+# VCG: 4.273511
+# SMT: 42.266685
+# Size: 194815 35
+# Result: 23 28
+# VCG: 9.663190
+# SMT: 104.948894
+# Size: 409587 49
+# Result: 2 3
+# VCG: 0.150750
+# SMT: 11.764220
+# Size: 11527 6
--- run_notrans.sh
+++ run_notrans.sh
@@ -21,3 +21,35 @@ run matrixMul.cu matrixMul
 run matrixMul.cu matrixMul2
 run matrixMul.cu matrixMul4
 run diffusion1d.cu diffusion1d
+
+
+## output
+
+# Result: 1 2
+# VCG: 0.003461
+# SMT: 6.714775
+# Size: 2527 2
+# Result: 1 3
+# VCG: 0.010778
+# SMT: 24.118131
+# Size: 3870 3
+# Result: 3 7
+# VCG: 0.017660
+# SMT: 28.904486
+# Size: 9196 7
+# Result: 15 17
+# VCG: 0.271460
+# SMT: 20.284746
+# Size: 26221 17
+# Result: 18 21
+# VCG: 2.743615
+# SMT: 56.095650
+# Size: 44842 21
+# Result: 23 29
+# VCG: 331.421013
+# SMT: 741.106044
+# Size: 445070 29
+# Result: 1 4
+# VCG: 192.522438
+# SMT: 8682.740279
+# Size: 10564478 4
